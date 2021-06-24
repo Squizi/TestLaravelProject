675c8bcb2de24443a757ad1e046f774e79161dc4
Nevena Pavlovska <nevencheto01@gmail.com>
2021-06-24 13:44:54 +0300

    Add card elements

--- resources/views/chart.blade.php
+++ resources/views/chart.blade.php
@@ -5,4 +5,48 @@
     <canvas id="myChart" width="400" height="200"></canvas>
 </div>
 
+<div class="container-cards">
+    <div class="card-people">
+        <img src="{{ asset('img/female.png') }}" class="card-people__image" alt="person">
+        <div class="card-text">
+            <h1>Samanhta Mils</h1>
+            <h5>Manager</h5>
+            <p>Some not so personal information about Samantha. But enough personal to get to know her a little bit. Hope this is helpfull.</p>
+        </div>
+        <ul class="social-icons">
+            <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+            <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+            <li><a href="#"><i class="fab fa-google-plus-square"></i></a></li>
+            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+        </ul>
+    </div>
+    <div class="card-people">
+        <img src="{{ asset('img/male.png') }}" class="card-people__image" alt="person">
+        <div class="card-text">
+            <h1>Anthony Floffs</h1>
+            <h5>Manager</h5>
+            <p>Some not so personal information about Anthony. But enough personal to get to know him a little bit. Hope this is helpfull.</p>
+        </div>
+        <ul class="social-icons">
+            <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+            <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+            <li><a href="#"><i class="fab fa-google-plus-square"></i></a></li>
+            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+        </ul>
+    </div>
+    <div class="card-people">
+        <img src="{{ asset('img/female.png') }}" class="card-people__image" alt="person">
+        <div class="card-text">
+            <h1>Samanhta Mils</h1>
+            <h5>Manager</h5>
+            <p>Some not so personal information about Samantha. But enough personal to get to know her a little bit. Hope this is helpfull.</p>
+        </div>
+        <ul class="social-icons">
+            <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+            <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+            <li><a href="#"><i class="fab fa-google-plus-square"></i></a></li>
+            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+        </ul>
+    </div>
+</div>
 @endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/styles.scss', 'public/css');
 
 mix.copy('node_modules/chart.js/dist/chart.js', 'public/chart.js/chart.js');
